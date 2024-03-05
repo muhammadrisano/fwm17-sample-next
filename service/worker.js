489d5baff08e269d8a1592605672c03ff8ec40fa
muhammadrisano <muhammadrisano@gmail.com>
2024-03-05 12:33:52 +0700

    add getworker

--- service/worker.js
+++ service/worker.js
@@ -1,18 +1,16 @@
 const url = process.env.NEXT_PUBLIC_API_URL
-import {cookies} from 'next/headers';
-const getCookie = async (name) => {
-  return cookies().get(name)?.value ?? '';
-}
+// import {cookies} from 'next/headers';
+// const getCookie = async (name) => {
+//   return cookies().get(name)?.value ?? '';
+// }
 
 export const getWorkers = async()=>{
   try {
-    const token = await getCookie('token')
+    // const token = await getCookie('token')
     const response = await fetch(`${url}/v1/workers/`,{
       headers: {
         "Content-Type": "application/json",
-        ...(token ? {"Cookie": `token=${token};path=/;expires=Session`}: {})
       },
-      credentials: "include",
 
     })
     const data = await response.json()
